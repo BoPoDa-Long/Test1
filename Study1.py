cbbef87ab74debc80470c834317c88831cd41a0f
Юрій <burdejnijurcik@gmail.com>
2025-10-06 01:28:15 +0300

    тест

--- Study1.py
+++ Study1.py
@@ -6,3 +6,13 @@ for i in menu:
 age = int(input("How old are you now?"))
 print("How old will you be next year?")
 print(age + 1)
+
+k='"Hello World"'
+print(k)
+def gfd():
+    a=7
+    return a
+a=gfd()
+b=8
+c=a+b
+print(c)
