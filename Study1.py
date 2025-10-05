c8dd8891b160506ed1575139f36bd423112cd7d8
Юрій <burdejnijurcik@gmail.com>
2025-10-06 01:14:11 +0300

    06.10.25/01:13

---
+++ Study1.py
@@ -0,0 +1,8 @@
+menu = ["wraps", "sandwiches", "soup", "salad"]
+x=0
+for i in menu:
+    x+=1
+    print(x,i)
+age = int(input("How old are you now?"))
+print("How old will you be next year?")
+print(age + 1)
